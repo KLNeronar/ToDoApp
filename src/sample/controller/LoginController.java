1373229bda2ab50eec6f1680ad39582bc0d2ab40
John Macken <john.wallace.macken@gmail.com>
2020-05-21 10:27:46 -0700

    Implemented the transition from the login page to sign up page.

--- src/sample/controller/LoginController.java
+++ src/sample/controller/LoginController.java
@@ -1,10 +1,15 @@
 package sample.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,9 +36,43 @@ public class LoginController {
     @FXML
     void initialize() {
 
-        loginButton.setOnAction(actionEvent -> {
-            System.out.println("Login Clicked");
+        String loginText = loginUsername.getText().trim();
+        String loginPwd = loginPassword.getText().trim();
+
+        loginSingupButton.setOnAction(actionEvent -> {
+
+            //Take users to signup screen
+            loginSingupButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/view/signup.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
         });
+
+        loginButton.setOnAction(actionEvent -> {
+
+            if (!loginText.equals("") || !loginPwd.equals("")) {
+                loginUser(loginText, loginPwd);
+            } else {
+                System.out.println("Error login in user!");
+            }
+        });
+    }
+
+    private void loginUser(String userName, String password) {
+
+        //Check in the database if the user exists
+        //If true, we take them to AddItem Screen
     }
 
 }
